73413b06524217eead1b946185741b4354115110
Daniel (OnFinality) <92831040+Danielvul@users.noreply.github.com>
2026-05-07 11:23:51 +1200

    Removed SubQuery's Extinct RPC
    No longer supported

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -986,7 +986,6 @@ export const prodRelayKusama: EndpointOption = {
     // RockX: 'wss://rockx-ksm.w3node.com/polka-public-ksm/ws', // https://github.com/polkadot-js/apps/issues/11477
     Spectrum: 'wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/QXq7QZ6Q60NDzA/kusama/mainnet/',
     Stakeworld: 'wss://rpc-kusama.stakeworld.io',
-    // SubQuery: 'wss://kusama.rpc.subquery.network/public/ws', https://github.com/polkadot-js/apps/issues/11026
     'light client': 'light://substrate-connect/kusama'
   },
   teleport: getTeleports(prodParasKusamaCommon),
